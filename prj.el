d9bbe026b8bb10c0305eb9eba80da3edc87d8d58
Anton Johansson <anton.johansson@gmail.com>
2008-11-08 18:47:50 +0100

    Added a dyniamic way to get classpath into emacs.

--- prj.el
+++ prj.el
@@ -2,9 +2,7 @@
 ;;                                      (expand-file-name "./bin/prod")
 ;;                                      (expand-file-name "./bin/test")
 ;;                                      (expand-file-name "./lib/junit-4.5.jar")))
-(set-variable 'jde-global-classpath '("./bin/prod"
-                                      "./bin/test"
-                                      "./lib/junit-4.5.jar"))
+(set-variable 'jde-global-classpath (my-get-classpath))
 
 ;;(setq jdibug-connect-host "localhost"
 ;;      jdibug-connect-port 5005)
