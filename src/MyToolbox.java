cb3500860211fa599a14fe193d51ce9ebd1d85b0
Anton Johansson <anton.johansson@gmail.com>
2008-11-17 11:45:17 +0100

    Revert "fix handledare"
    This reverts commit 8fbedb4b109f2c67807196c5b32c53af6a78d328.

--- src/MyToolbox.java
+++ src/MyToolbox.java
@@ -1,6 +1,6 @@
 /*
  * @(#)MyToolbox.java
- * Time-stamp: "2008-11-17 10:40:06 anton"
+ * Time-stamp: "2008-11-16 23:52:40 anton"
  */
 
 import java.awt.BorderLayout;
@@ -161,14 +161,16 @@ public class MyToolbox extends JFrame {
             logger.info("Method returned: " + ret);
         } catch (IllegalArgumentException e) {
             // The method has been passed illegal or inappropriate
-            // arguments, too many or too few.
+            // arguments.
             System.out.println(e.getMessage());
         } catch (InvocationTargetException e) {
-            System.out.println("Parameters can't be converted from the supplied"
-                               + " String to the required class.");
+            System.out.println("Parameters can't be converted from a String to"
+                               + " the required class");
         } catch (NoSuchMethodException e) {
-            System.out.println("The paramters needed to invoke this method"
-                               + " can't be created from a String.");
+            // The class stored in classHandler doesn't have the specified
+            // method. This should never happen.
+            e.printStackTrace();
+            System.exit(-1);
         } catch (InstantiationException e) {
             // Should never happen, NoSuchMethodException should be thrown
             // first.
